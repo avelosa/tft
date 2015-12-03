ffd6130c2513941e189e1a6c71954921574d87bb
ptchizek <ptchizek@mtu.edu>
2015-12-03 15:02:25 -0500

    Basic uploading working.
    On upload it will stick the uploaded file into the uploaded folder.  The
    /upload page displays information about the uploaded file, obviously
    this will need to be changed.

--- app.go
+++ app.go
@@ -6,8 +6,43 @@ import (
 	"net/http"
 	"os/user"
 
+	"io"
+	"time"
+	"os"
+	"strconv"
+	"crypto/md5"
+
 	"github.com/gorilla/mux"
 )
+
+func upload(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("method:", r.Method)
+    if r.Method == "GET" {
+        crutime := time.Now().Unix()
+        h := md5.New()
+        io.WriteString(h, strconv.FormatInt(crutime, 10))
+        token := fmt.Sprintf("%x", h.Sum(nil))
+
+        t, _ := template.ParseFiles("upload.gtpl")
+        t.Execute(w, token)
+    } else {
+        r.ParseMultipartForm(32 << 20)
+        file, handler, err := r.FormFile("uploadfile")
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer file.Close()
+        fmt.Fprintf(w, "%v", handler.Header)
+        f, err := os.OpenFile("./uploaded/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer f.Close()
+        io.Copy(f, file)
+    }
+}
 
 // Load in the templates we need
 var templates = template.Must(template.ParseFiles("templates/home.html"))
@@ -31,6 +66,7 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
+	r.HandleFunc("/upload", upload)
 
 	http.ListenAndServe(":3000", r)
 }
